73f966f285729e2e27b2080f4cd9abf0a900b676
v_lianqingwei <v_lianqingwei@baidu.com>
2021-11-01 11:17:14 +0000

    listen ctl-c for use endless module

--- main.go
+++ main.go
@@ -2,22 +2,39 @@ package main
 
 import (
 	"fmt"
-	"net/http"
+	"log"
+	"syscall"
 
 	"github.com/aklian/go-gin-example/pkg/setting"
 	"github.com/aklian/go-gin-example/routers"
+	"github.com/fvbock/endless"
 )
 
 func main() {
-	router := routers.InitRouter()
+	endless.DefaultReadTimeOut = setting.ReadTimeout
+	endless.DefaultWriteTimeOut = setting.WriteTimeout
+	endless.DefaultMaxHeaderBytes = 1 << 20
+	endPoint := fmt.Sprintf(":%d", setting.HTTPPort)
 
-	s := &http.Server{
-		Addr: fmt.Sprintf(":%d", setting.HTTPPort),
-		Handler: router,
-		ReadTimeout: setting.ReadTimeout,
-		WriteTimeout: setting.WriteTimeout,
-		MaxHeaderBytes: 1 << 20,
+	server := endless.NewServer(endPoint, routers.InitRouter())
+	server.BeforeBegin = func(add string) {
+		log.Printf("actual pid is %d", syscall.Getpid())
 	}
 
-	s.ListenAndServe()
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Printf("Server err: %v", err)
+	}
+
+	// router := routers.InitRouter()
+
+	// s := &http.Server{
+	// 	Addr: fmt.Sprintf(":%d", setting.HTTPPort),
+	// 	Handler: router,
+	// 	ReadTimeout: setting.ReadTimeout,
+	// 	WriteTimeout: setting.WriteTimeout,
+	// 	MaxHeaderBytes: 1 << 20,
+	// }
+
+	// s.ListenAndServe()
 }
